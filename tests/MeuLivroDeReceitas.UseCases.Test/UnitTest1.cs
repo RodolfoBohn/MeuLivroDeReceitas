d0f9ca45c8223600486893dbebdcc2a99ee684a4
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2023-07-13 16:39:33 -0300

    First Commit

---
+++ tests/MeuLivroDeReceitas.UseCases.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MeuLivroDeReceitas.UseCases.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/MeuLivroDeReceitas.Validators.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MeuLivroDeReceitas.Validators.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
